f5cba6ae3d5417a4513b5babb7e12bbc571455f2
Ben Sokol <ben@bensokol.com>
2019-02-11 11:42:43 -0600

    Update .gitignore and add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/**
+* @Filename: main.cpp
+* @Author:   Ben Sokol
+* @Email:    ben@bensokol.com
+* @Created:  February 11th, 2019 [11:15am]
+* @Modified: February 11th, 2019 [11:39am]
+* @Version:  1.0.0
+*
+* Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
+*/
+
+#include <iostream>
+
+int main() {
+  return 0;
+}
