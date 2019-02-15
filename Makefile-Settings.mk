a44d357f614467e7f4d6121997b52686c1de3bf8
Ben Sokol <ben@bensokol.com>
2019-02-14 20:25:30 -0600

    Move Makefile to submodule build-tools

---
+++ Makefile-Settings.mk
@@ -0,0 +1,23 @@
+# @Filename: Makefile-Settings.mk
+# @Author:   Ben Sokol <Ben>
+# @Email:    ben@bensokol.com
+# @Created:  February 14th, 2019 [5:21pm]
+# @Modified: February 14th, 2019 [5:51pm]
+# @Version:  1.0.0
+#
+# Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
+
+###############################################################################
+# Makefile Settings                                                           #
+###############################################################################
+EXE = battleship
+
+CXXSTD = -std=c++14
+
+CXXFLAGS = -O3 -g
+WARNING_FLAGS = -Wall -Wextra -Wpedantic
+
+MACOS_FLAGS = -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded -Wno-date-time
+LINUX_FLAGS = -pthread
+
+COMMAND_LINE_ARGS = 4 3 4
