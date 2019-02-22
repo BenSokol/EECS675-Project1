4463711449e3497ee7d3bf27806855f4e9e60595
Ben Sokol <ben@bensokol.com>
2019-02-22 11:19:40 -0600

    Update submodules/build-tools

--- Makefile-Settings.mk
+++ Makefile-Settings.mk
@@ -2,7 +2,7 @@
 # @Author:   Ben Sokol <Ben>
 # @Email:    ben@bensokol.com
 # @Created:  February 14th, 2019 [5:21pm]
-# @Modified: February 21st, 2019 [3:11am]
+# @Modified: February 21st, 2019 [1:46pm]
 # @Version:  1.0.0
 #
 # Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
@@ -35,3 +35,8 @@ MAKEFILE_RULE_INSTALL_DISABLE = 1
 MAKEFILE_RULE_UNINSTALL_DISABLE = 1
 
 ENABLE_LOGGING = 1
+
+CPPCHECK_DEFINES = -D__cplusplus -DENABLE_LOGGING
+CPPCHECK_SUPPRESS = --suppress=missingIncludeSystem --suppress=preprocessorErrorDirective:submodules/utilities/UTL_textWrap.cpp:17
+CPPCHECK_FLAGS = -q --platform=native --error-exitcode=1 --force --std=c11 --std=c++11
+CPPCHECK_ENABLE = --enable=warning --enable=style --enable=missingInclude
